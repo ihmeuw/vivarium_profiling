2402a37924105d43b25820615723df1eb4e2dc26
Patrick Nast <pnast@uw.edu>
2025-12-31 13:51:10 -0800

    add custom artifact manager

---
+++ src/vivarium_profiling/plugins/artifact.py
@@ -0,0 +1,24 @@
+from vivarium.framework.artifact import ArtifactManager as VivariumArtifactManager
+import re
+
+
+class ArtifactManager(VivariumArtifactManager):
+    """Customized ArtifactManager for Vivarium Profiling.
+
+    The Key difference is that when calling builder.data.load,
+    we will strip the numeric suffix from the cause/risk name in the data key.
+    """
+
+    def load(self, key: str):
+        # Strip numeric suffix from cause/risk names
+        # e.g. 'risk_factor.high_systolic_blood_pressure_1.distribution'
+        # to 'risk_factor.high_systolic_blood_pressure.distribution'
+
+        breakpoint()
+        if len(key.split(".")) != 3:
+            return super().load(key)
+
+        entity_type, entity, measure = key.split(".")
+        modified_entity = re.sub(r"(_\d+)$", "", entity)
+        modified_key = f"{entity_type}.{modified_entity}.{measure}"
+        return super().load(modified_key)

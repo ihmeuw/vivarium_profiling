89e4decf66816bf326305ada1e34bc308b0e18e7
Patrick Nast <130876799+patricktnast@users.noreply.github.com>
2026-01-02 09:41:46 -0800

    Define new artifact manager (#5)
    * add custom artifact manager
    
    * fix tests
    
    * rearrange
    
    * format
    
    * format
    
    * Refine docstring and remove debugging breakpoint
    
    Updated docstring for clarity and removed breakpoint.
    
    * Update src/vivarium_profiling/plugins/artifact.py
    
    Co-authored-by: Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>

---
+++ src/vivarium_profiling/plugins/artifact.py
@@ -0,0 +1,24 @@
+import re
+
+from vivarium.framework.artifact import ArtifactManager as VivariumArtifactManager
+
+
+class ArtifactManager(VivariumArtifactManager):
+    """Customized ArtifactManager for Vivarium Profiling.
+
+    The key difference is that when calling builder.data.load,
+    we will strip the numeric suffix from the cause/risk name in the data key.
+    """
+
+    def load(self, key: str):
+        """Strip numeric suffix from cause/risk names
+        e.g.: 'risk_factor.high_systolic_blood_pressure_1.distribution'
+        -> 'risk_factor.high_systolic_blood_pressure.distribution'
+        """
+        if len(key.split(".")) != 3:
+            return super().load(key)
+
+        entity_type, entity, measure = key.split(".")
+        modified_entity = re.sub(r"(_\d+)$", "", entity)
+        modified_key = f"{entity_type}.{modified_entity}.{measure}"
+        return super().load(modified_key)

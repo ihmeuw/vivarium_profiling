8dd4d0f87d1d6fc4f777d74880b0da92564d51a8
Patrick Nast <pnast@uw.edu>
2026-01-02 09:42:36 -0800

    subclass risk effects

--- src/vivarium_profiling/plugins/parser.py
+++ src/vivarium_profiling/plugins/parser.py
@@ -7,7 +7,12 @@ from vivarium_public_health.disease.models import SIS_fixed_duration
 from vivarium_public_health.results import DiseaseObserver
 from vivarium_public_health.results.risk import CategoricalRiskObserver
 from vivarium_public_health.risks.base_risk import Risk
-from vivarium_public_health.risks.effect import NonLogLinearRiskEffect, RiskEffect
+from vivarium_public_health.risks.effect import (
+    NonLogLinearRiskEffect as NonLogLinearRiskEffect_,
+    RiskEffect as RiskEffect_,
+)
+import pandas as pd
+import re
 
 CAUSE_KEY = "causes"
 RISK_KEY = "risks"
@@ -17,6 +22,34 @@ DEFAULT_RISK_CONFIG = {
     "observers": False,
     "affected_causes": {},
 }
+
+
+class RiskEffect(RiskEffect_):
+
+    def get_filtered_data(
+        self, builder: "Builder", data_source: str | float | pd.DataFrame
+    ) -> float | pd.DataFrame:
+        data = super().get_data(builder, data_source)
+
+        if isinstance(data, pd.DataFrame):
+            # filter data to only include the target entity and measure
+            correct_target_mask = True
+            columns_to_drop = []
+            if "affected_entity" in data.columns:
+                # THIS IS THE ONLY CHANGE! We need to filter to the non-suffixed name
+                correct_target_mask &= data["affected_entity"] == re.sub(
+                    r"(_\d+)$", "", self.target.name
+                )
+                columns_to_drop.append("affected_entity")
+            if "affected_measure" in data.columns:
+                correct_target_mask &= data["affected_measure"] == self.target.measure
+                columns_to_drop.append("affected_measure")
+            data = data[correct_target_mask].drop(columns=columns_to_drop)
+        return data
+
+
+class NonLogLinearRiskEffect(NonLogLinearRiskEffect_, RiskEffect):
+    pass
 
 
 class MultiComponentParsingErrors(ParsingError):
